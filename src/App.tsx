87a084499d8649bc4f2cb3dd8293b3556a0d5bdc
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2023-05-15 20:20:59 +0200

    Ändern des Titels mit DB Logo

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,22 @@ import {
 import './App.css'
 import Board from './components/Board'
 
-import { Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
 
 function App() {
   return (
     <>
       <Typography variant='h2' align='center' style={{ paddingBottom: '10px' }}>
-        DB Case Study Chess
+        <Box display='flex' alignItems='center' justifyContent='center'>
+          <img
+            src='/Deutsche_Bahn_AG-Logo.svg.png'
+            alt='DB Logo'
+            style={{ height: '45px', paddingRight: '15px' }}
+          />
+          Case Study Chess
+        </Box>
       </Typography>
       <div className='App'>
         <Router>
