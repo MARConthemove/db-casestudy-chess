bb1c68433dceda3b18b0e5585fdd172d6190573d
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2023-05-15 12:25:08 +0200

    React-Router wurde eingebunden

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Board from './components/board'
 
@@ -10,6 +11,9 @@ const App: React.FC = () => {
       <Typography variant='h2' align='center' style={{ paddingBottom: '10px' }}>
         DB Case Study Chess
       </Typography>
+      <Routes>
+        <Route path='/' />
+      </Routes>
       <div className='App'>
         <Board />
       </div>
